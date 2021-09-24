e872125a9865ed77217f137b65adb1fe4ad8ef98
canavar <bartucanavar@gmail.com>
2021-09-24 20:41:44 +0300

    sa

---
+++ app.js
@@ -0,0 +1,9 @@
+function abdest() {
+     let img = document.getElementById("img")   
+     img.src = "assets/feels.png";
+     let txt = document.getElementById("txt")
+     txt.innerHTML = "Apdestlendin"
+     let audio = document.getElementById("audio")
+     audio.play();
+     console.log("ABDESSTTT")
+}
